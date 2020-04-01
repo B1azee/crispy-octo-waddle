7b4a30223c8b9130c241cb35a9e75fa8ec6e6d48
B1azee <57710788+B1azee@users.noreply.github.com>
2020-04-02 01:19:57 +0500

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void plus1 (int *pa)
+{
+ (*pa)++; / / adding 1 to the variable by the allocated pointer
+}
+
+int main()
+{
+ int a = 4;
+ int *pa = &a; / / creating a pointer; assigning it the address of the variable " a"
+ cout << "address a :" << pa << endl;
+ cout << "a =" << a << endl;
+ cout << "a =" << *pa << endl; // writing the variable "a" through the pointer
+ cout << "================================== " << endl;
+
+ plus1(pa); / / passing the pointer to the function
+
+ cout << "a =" << a << endl;
+ cout << "================================== " << endl;
+
+ plus1(&a);// passing the address of the variable to the function
+
+ cout << "a =" << a << endl;
+}
